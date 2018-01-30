43cee9103846a6588062954fb38844bb8d6cbdbb
Ivo Alxneit <ivo.alxneit@psi.ch>
2018-01-30 08:48:12 +0100

    add copyright (GPL3)
    Signed-off-by: Ivo Alxneit <ivo.alxneit@psi.ch>

--- HRTEM/HRTEMFringeAnalyzer.py
+++ HRTEM/HRTEMFringeAnalyzer.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Sep 25 12:05:26 2017
+#
+# Copyright 2017-2018 Ivo Alxneit, Paul Scherrer Institut
+#
+# This file is part of  HRTEMFringeAnalyzer.
+#
+# HRTEMFringeAnalyzer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# HRTEMFringeAnalyzer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with HRTEMFringeAnalyzer. If not, see <http://www.gnu.org/licenses/>.
 
-@author: alxneit
-"""
 # pylint: disable=C0103
 import sys
 import warnings
